56e45755096c2fa7c70c284222e692dda3d12da8
Eranda Madusanka <madushankaeranda56@gmail.com>
2025-03-30 23:44:35 +0530

    update2

---
+++ login.php
@@ -0,0 +1,47 @@
+<?php
+session_start();
+include 'db_connect.php';
+
+if(isset($_POST['login'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    
+    $stmt = $conn->prepare("SELECT * FROM users WHERE email = ?");
+    $stmt->execute([$email]);
+    $user = $stmt->fetch();
+    
+    if($user && password_verify($password, $user['password'])) {
+        $_SESSION['user_id'] = $user['id'];
+        $_SESSION['username'] = $user['username'];
+        header("Location: home.php");
+    } else {
+        $error = "Invalid credentials";
+    }
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="container">
+        <h2>Login</h2>
+        <?php if(isset($error)) echo "<p style='color:red'>$error</p>"; ?>
+        <form method="POST">
+            <div class="form-group">
+                <input type="email" name="email" placeholder="Email" required>
+            </div>
+            <div class="form-group">
+                <input type="password" name="password" placeholder="Password" required>
+            </div>
+            <button type="submit" name="login">Login</button>
+        </form>
+        <div class="social-login">
+            <p><a href="#">Login with Google</a> | <a href="#">Login with Facebook</a></p>
+            <p><a href="signup.php">Sign Up</a> | <a href="forgot.php">Forgot Password?</a></p>
+        </div>
+    </div>
+</body>
+</html>
